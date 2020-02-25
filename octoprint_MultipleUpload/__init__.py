cd78ee72c1451958d677e9b624fe8283b79dba58
nhchiu <nhchiu2009@gmail.com>
2020-02-26 01:07:59 +0800

    Add compatibility with Octoprint-1.4.0rc5 and Python-3.7

--- octoprint_MultipleUpload/__init__.py
+++ octoprint_MultipleUpload/__init__.py
@@ -41,6 +41,7 @@ class MultipleUploadPlugin(octoprint.plugin.AssetPlugin):
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
 __plugin_name__ = "MultipleUpload Plugin"
+__plugin_pythoncompat__ = ">=2.7,<4"
 
 def __plugin_load__():
   global __plugin_implementation__
